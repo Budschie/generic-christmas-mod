db14a3952c4c1f218ed9014b35e14ea77b8ee8fd
Budschie <jul.kp06@gmail.com>
2021-12-05 14:25:33 +0100

    Replaced examplemod with the appropriate mod id

---
+++ src/main/java/de/budschie/generic_christmas_mod/GenericChristmasMod.java
@@ -0,0 +1,19 @@
+package de.budschie.generic_christmas_mod;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.fml.common.Mod;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("generic_christmas_mod")
+public class GenericChristmasMod
+{
+	// Directly reference a log4j logger.
+	private static final Logger LOGGER = LogManager.getLogger();
+
+	public GenericChristmasMod()
+	{
+
+	}
+}
